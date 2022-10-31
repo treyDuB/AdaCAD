925ae085d2a0f4acc73e595e107ff21d9d8c73b8
lawking87 <47283667+lawking87@users.noreply.github.com>
2022-10-30 18:13:40 -0600

    Revert "Merge branch 'master' into pedals"
    This reverts commit fbdf8d993e4bf550b5a2f3b10a1e77a755d9cc14, reversing
    changes made to 3f28b3173d09f74838f4176d5fb659f2152151b7.

--- src/app/core/modal/loom/loom.modal.spec.ts
+++ src/app/core/modal/loom/loom.modal.spec.ts
@@ -1,16 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-<<<<<<<< HEAD:src/app/weaver/modal/selvedge/selvedge.modal.spec.ts
-import { SelvedgeModal } from './selvedge.modal';
-
-describe('SelvedgeModal', () => {
-  let component: SelvedgeModal;
-  let fixture: ComponentFixture<SelvedgeModal>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SelvedgeModal ]
-========
 import { LoomModal } from './loom.modal';
 
 describe('LoomModal', () => {
@@ -20,17 +9,12 @@ describe('LoomModal', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ LoomModal ]
->>>>>>>> upstream/master:src/app/core/modal/loom/loom.modal.spec.ts
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-<<<<<<<< HEAD:src/app/weaver/modal/selvedge/selvedge.modal.spec.ts
-    fixture = TestBed.createComponent(SelvedgeModal);
-========
     fixture = TestBed.createComponent(LoomModal);
->>>>>>>> upstream/master:src/app/core/modal/loom/loom.modal.spec.ts
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/mixer/modal/ophelp/ophelp.modal.spec.ts
+++ src/app/mixer/modal/ophelp/ophelp.modal.spec.ts
@@ -1,16 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-<<<<<<<< HEAD:src/app/weaver/modal/shape/shape.modal.spec.ts
-import { ShapeModal } from './shape.modal';
-
-describe('PatternModal', () => {
-  let component: ShapeModal;
-  let fixture: ComponentFixture<ShapeModal>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ShapeModal ]
-========
 import { OpHelpModal } from './ophelp.modal';
 
 describe('OpHelpModal', () => {
@@ -20,17 +9,12 @@ describe('OpHelpModal', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ OpHelpModal ]
->>>>>>>> upstream/master:src/app/mixer/modal/ophelp/ophelp.modal.spec.ts
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-<<<<<<<< HEAD:src/app/weaver/modal/shape/shape.modal.spec.ts
-    fixture = TestBed.createComponent(ShapeModal);
-========
     fixture = TestBed.createComponent(OpHelpModal);
->>>>>>>> upstream/master:src/app/mixer/modal/ophelp/ophelp.modal.spec.ts
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
